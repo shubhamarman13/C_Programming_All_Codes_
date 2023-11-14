253cf55a81a48614db6ae17ffa2e6e34c47d15d7
shubhamarman13 <shubhamarman13@gmail.com>
2023-11-14 19:41:48 +0530

    sqaue_of_numbers

---
+++ sqaue_of_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+  int a,b,c;
+  printf("enter the number \n");
+  scanf("%d", &a);
+  printf("enter the number \n");
+  scanf("%d", &b);
+  printf("enter the number \n");
+  scanf("%d", &c);
+  
+  printf("The squar eof the number is %d \n", a*b*c);
+  printf(" I HAVE DONE THE FIRST CODE FROM C PROGRAMMING ;- SHUBHAM ARMAN");
+
+    return 0;
+
+} 
+  
