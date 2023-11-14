bad683fa1cb7f75f5fbd3237d0a278c593ab4cb3
shubhamarman13 <shubhamarman13@gmail.com>
2023-11-14 19:23:46 +0530

    code_to_add_a_int_and_a_float updated

---
+++ Basic Codes/code_to_add_a_int_and_a_float.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int num1;
+	float num2;
+	printf("ENTER A INTEGER  = \n");
+	scanf("%d",&num1);
+	printf("ENTER A  FLOAT. = \n");
+	scanf("%f",&num2);
+	printf("SUM OF %d & %0.2f  = %0.2f\n",num1,num2,num1+num2);
+	
+}
