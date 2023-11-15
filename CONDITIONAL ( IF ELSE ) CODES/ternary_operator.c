aeebca8c3823a05915c5fc436e06909ae94ed422
shubhamarman13 <shubhamarman13@gmail.com>
2023-11-16 00:03:33 +0530

    ternary_operator

---
+++ CONDITIONAL ( IF ELSE ) CODES/ternary_operator.c
@@ -0,0 +1,9 @@
+// EXAMPLE OF IF ELSE IN ONE LINE//
+// CODE TO FIND THE NUMBER IS GREATER THAN 10 OR NOT //
+#include <stdio.h>
+void  main()
+{int a;
+printf("enter the number \n");
+scanf("%d", &a);
+(a>10) ? printf(" %d is greater then 10",a) : printf("%d is less then 10",a);
+}
